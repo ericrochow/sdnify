eab6d2faf3d350436938e96a1e0b7d4553068187
Eric Rochow <erochow@liquidweb.com>
2019-08-05 08:11:24 -0400

    seed with better info and version number

--- __version__.py
+++ __version__.py
@@ -1,6 +1,9 @@
 __author__ = "Eric Rochow"
 __author_email__ = "ericrochow@gmail.com"
-__description__ = ""
+__description__ = (
+    "Pulls device info from a variety of vendors and returns it in a"
+    " standardized format."
+)
 __title__ = "sdnify"
-__url__ = ""
-__version__ = "0.1.0"
+__url__ = "https://github.com/ericrochow/sdnify"
+__version__ = "2019.08.05"
