84b65c579236c6e12b88f9df8cb67412c0834afd
Eric Rochow <erochow@liquidweb.com>
2019-08-06 10:10:32 -0400

    reorganize setup.py and look for __version__.py in sdnify dir instead of base dir

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from io import open
 import os
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-import __version__
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+VERSION_FILE = os.path.join(HERE, "/VERSION")
 
 with open("README.md", mode="r", encoding="utf-8") as f:
     readme = f.read()
@@ -15,6 +18,53 @@ with open("reqs/requirements.txt") as reqs:
     requires = reqs.read().splitlines()
 with open("reqs/requirements-test.txt") as reqs:
     test_requires = reqs.read().splitlines()
+
+
+def find_current_version():
+    """
+    Reads the current version number from the VERSION file.
+
+    Args:
+      None
+    Returns:
+      A string containing the current version number.
+    """
+    with open(VERSION_FILE) as v:
+        return v.read()
+
+
+def update_version(current, new_version):
+    """
+    Increments the version number.
+
+    Args:
+      None
+    Returns:
+      A string containing the updated version number.
+    """
+    with open(VERSION_FILE, "w") as v:
+        for line in v:
+            v.write(new_version)
+        return find_current_version()
+
+
+def generate_version():
+    """
+    """
+    date = datetime.now()
+    year = date.year
+    month = date.month
+    day = date.day
+    current_version = find_current_version()
+    if current_version.startswith("{}.{}.{}".format(year, month, day)):
+        minor_version = int(current_version[-1]) + 1
+        new_version = update_version(
+            "{}.{}.{}.{}".format(year, month, date, minor_version)
+        )
+    else:
+        new_version = update_version("{}.{}.{}.0".format(year, month, day))
+    return new_version
+
 
 if sys.argv[-1] == "publish_dev":
     os.system("python setup.py sdist bdist_wheel")
@@ -50,25 +100,16 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
-packages = ["sdnify"]
+about = {}
+with open(os.path.join(HERE, "sdnify", "__version__.py"), "r", "utf-8") as f:
+    exec(f.read(), about)
+
+packages = find_packages(exclude=["tests"])
+version = find_current_version()
 
 setup(
-    name=__version__.__title__,
-    version=__version__.__version__,
-    author=__version__.__author__,
-    author_email=__version__.__author_email__,
-    url=__version__.__url__,
-    packages=packages,
-    package_data={"": ["LICENSE", "NOTICE"], "sdnify": [""]},
-    package_dir={"sdnify": "sdnify"},
-    include_package_data=True,
-    python_requires=">=3.5",
-    install_requires=requires,
-    license=[""],
-    zip_safe=False,
-    description=__version__.__description__,
-    long_description=readme,
-    scripts=["sdnify/cli.py"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -79,9 +120,25 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    tests_require=test_requires,
     cmdclass={"test": PyTest},
+    description=about["__description__"],
+    download_url="{}/archive/{}.zip".format(about["__github_url__"], version),
     extras_require={
         "docs": ["Sphinx", "SimpleHTTPServer", "sphinx_rtd_theme"]
     },
+    entry_points={"console_scripts": ["sdnify = sdnify.interfaces.cli:main"]},
+    include_package_data=True,
+    install_requires=requires,
+    license=[""],
+    long_description=readme,
+    name=about["__title__"],
+    package_data={"": ["LICENSE", "NOTICE"], "sdnify": [""]},
+    package_dir={"sdnify": "sdnify"},
+    packages=packages,
+    project_urls=about["__urls__"],
+    python_requires=">=3.5",
+    tests_require=test_requires,
+    url=about["__url__"],
+    version=generate_version(),
+    zip_safe=False,
 )
